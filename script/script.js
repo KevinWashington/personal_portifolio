8047fb296245d496d92e7592cc57b76d5913341a
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2022-02-10 23:07:06 -0300

    Add files via upload
    Parallax Effect and menu navigation.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,24 @@
+let moon = document.querySelector('#moon')
+let stars = document.querySelector('#stars')
+let mountains_behind = document.querySelector('#mountains_behind')
+let mountains_front = document.querySelector('#mountains_front')
+let goleft = document.querySelector('#goleft')
+let gorigth = document.querySelector('#gorigth')
+let btn = document.querySelector('#btn')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value*0.25 + 'px'
+    moon.style.top = value*1.05 + 'px'
+    mountains_behind.style.top = value*0.5 + 'px'
+    mountains_front.style.top = value*0 + 'px'
+    goleft.style.marginRight = value*4 + 'px'
+    //goleft.style.marginTop = value*1 + 'px'
+    gorigth.style.marginLeft = value*4 + 'px'
+    //gorigth.style.marginTop = value*1 + 'px'
+    btn.style.marginTop = value*1.5 + 'px' 
+})
+
 function htmlinfo(){
   document.querySelector("#js").style.boxShadow = "none"
   document.querySelector("#css").style.boxShadow = "none"
@@ -18,4 +39,4 @@ function jsinfo(){
   document.getElementById("infoling").innerText = "JavaScript é uma linguagem de programação interpretada estruturada, de script de alto nível com tipagem dinâmica fraca e multiparadigma."
   document.querySelector("#js").style.boxShadow = "0px 0px 15px var(--selectcor)"
 }
-
+//Parallax
